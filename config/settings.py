d9530b3057b7e36fa0a045dea1a0c4b3b8e72631
georgfoe <38016206+georgfoe@users.noreply.github.com>
2024-10-02 10:01:03 +0200

    Eingrenzung auf Bundesweite Förderprogramme
    Die Eingrenzung wurde über eine Anpassung der Base-URL vorgenommen.

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
 ﻿# config/settings.py
 
-BASE_URL = 'https://www.foerderdatenbank.de/SiteGlobals/FDB/Forms/Suche/Foederprogrammsuche_Formular.html?filterCategories=FundingProgram'
+BASE_URL = 'https://www.foerderdatenbank.de/SiteGlobals/FDB/Forms/Suche/Foederprogrammsuche_Formular.html?cl2Processes_Foerdergebiet=_bundesweit&filterCategories=FundingProgram'
 CACHE_NAME = 'data/cache/foerderdatenbank_cache'
 CACHE_BACKEND = 'sqlite'
 CACHE_EXPIRE = None
