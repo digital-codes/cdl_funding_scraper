0b8cc4f82cb3ba5a0bcadad580d41dd3cbc97b37
Frie Preu <fr1e@pm.me>
2024-11-14 12:35:18 +0100

    quickfix for #2

--- src/parser.py
+++ src/parser.py
@@ -14,25 +14,32 @@ def parse_program_page(html_bytes: bytes, url: str) -> dict:
     """
     tree = lxml.html.fromstring(html_bytes)
 
-    # titel des Förderprogramms
+    # title des Förderprogramms
     title = tree.xpath("//h1[@class='title']")[0].text_content().strip()
 
     articles = {}
     article_nodes = tree.xpath("//div[@class='content']//article")
-    for article in article_nodes:
-        tab_id = article.attrib.get("id")
-        if tab_id:
-            tab_name = (
-                tree.xpath(f'//h2[@data-toggleid="{tab_id}"]')[0].text_content().strip()
-            )
-        else:
-            node = article.getprevious()
-            assert node.tag == "h2"
-            tab_name = node.text_content().strip()
-        content = lxml.html.tostring(article, encoding="unicode")
-        if tab_name in articles:
-            raise Exception(f"Duplicate tab name: {tab_name}")
-        articles[tab_name] = content
+    if (len(article_nodes) > 0):
+        for article in article_nodes:
+            tab_id = article.attrib.get("id")
+            if tab_id:
+                tab_name = (
+                    tree.xpath(f'//h2[@data-toggleid="{tab_id}"]')[0].text_content().strip()
+                )
+            else:
+                node = article.getprevious()
+                assert node.tag == "h2"
+                tab_name = node.text_content().strip()
+            content = lxml.html.tostring(article, encoding="unicode")
+            if tab_name in articles:
+                raise Exception(f"Duplicate tab name: {tab_name}")
+            articles[tab_name] = content
+    else:
+        # this page does not have "tabs" - very hacky workaround
+        content_node = tree.xpath("//main/div[@class='jumbotron']/following-sibling::div/div[@class='content']")[0]
+        content = lxml.html.tostring(content_node, encoding="unicode")
+        # quickfix : construct artificial articles object with "Kurzzusammenfassung" 
+        articles = [{"Kurzzusammenfassung": content}]
 
     details = {}
     dt_elements = tree.xpath("//dt")
