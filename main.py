69100de582ed23c99ca2188f8f6e0b78028a9b3e
jstet <mail@jstet.net>
2025-04-23 17:50:36 +0200

    back to old dlt

--- main.py
+++ main.py
@@ -79,7 +79,6 @@ def crawl():
     scraping_host.run(
         columns=FundingProgramSchema,
         write_disposition={
-            "primary_key": "id_hash",
             "disposition": "merge",
             "strategy": "scd2",
             "validity_column_names": ["on_website_from", "on_website_to"],
